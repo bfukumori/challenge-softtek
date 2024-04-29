a0d967806cf327207531c9f97326c6fdd33c2c1a
Bruno Fukumori <brunofukumori@gmail.com>
2024-04-29 01:06:10 -0300

    add base components and change folder structure

---
+++ src/app/(dashboard)/@admin/incidents/page.tsx
@@ -0,0 +1,7 @@
+export default function Incidents() {
+  return (
+    <main>
+      <h1>Chamados</h1>
+    </main>
+  );
+}
--- src/app/dashboard/@user/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Header } from '@/components/header';
-
-export default function UserDashboard() {
-  return (
-    <main>
-      <h1>User Dashboard</h1>
-    </main>
-  );
-}
