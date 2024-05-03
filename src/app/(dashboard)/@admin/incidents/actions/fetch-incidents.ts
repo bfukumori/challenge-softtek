8dfa36c01673b38198db4c6f0f8086b881971057
Bruno Araujo <brunoandradel.a.dev@gmail.com>
2024-05-02 21:41:12 -0300

    :sparkles: fear: Page Dashboard User

--- src/app/(dashboard)/@admin/incidents/actions/fetch-incidents.ts
+++ src/app/(dashboard)/@admin/incidents/actions/fetch-incidents.ts
@@ -1,11 +1,11 @@
 import {
   GetIncidentsResponseType,
   IncidentStatusType,
-} from '@/@types/incidents';
+} from "@/@types/incidents";
 
 export interface FetchIncidentsParams {
   page: string;
-  status: IncidentStatusType;
+  status: IncidentStatusType | "";
   incidentId: string;
 }
 
@@ -14,28 +14,28 @@ export async function fetchIncidents({
   status,
   incidentId,
 }: FetchIncidentsParams): Promise<GetIncidentsResponseType> {
-  const url = new URL('http://localhost:3000/api/incidents');
+  const url = new URL("http://localhost:3000/api/incidents");
 
   if (page) {
-    url.searchParams.append('page', page);
+    url.searchParams.append("page", page);
   }
 
   if (status) {
-    url.searchParams.append('status', status);
+    url.searchParams.append("status", status);
   }
 
   if (incidentId) {
-    url.searchParams.append('incidentId', incidentId);
+    url.searchParams.append("incidentId", incidentId);
   }
 
   const response = await fetch(url.toString(), {
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
   });
 
   if (!response.ok) {
-    throw new Error('Failed to fetch data');
+    throw new Error("Failed to fetch data");
   }
 
   return response.json();
