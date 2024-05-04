17553db113cf5a606a1ab8a2f264cad53aae1a29
bruperdd <bperdigaosantos@gmail.com>
2024-05-03 22:05:32 -0300

    :sparkles: feat: Pagina de Login

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,3 +1,34 @@
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function SignIn() {
-  return <main>Sign In</main>;
+  return (
+    <main className="p-80 bg-current">
+      <Card className="p-4">
+      <div className="flex justify-center">
+          <Image src="/logotipo.png" width={200} height={200} alt="Logo Softtek"/>
+        </div>
+      <div className="mt-8 mx-12">
+        <Label className="">Digite seu e-mail:</Label>
+        <Input placeholder="E-mail"/>
+        <Label>Digite sua senha:</Label>
+        <Input placeholder="Senha"/>
+        <div className="flex justify-center">
+            <Button className="mt-4 px-24">Login</Button>
+        </div>
+        <div>
+          <Label className="mt-8 flex justify-center">Esqueceu sua senha?</Label>
+        </div>
+      </div>
+      </Card>
+      <Card className="py-4 mt-4 text-center">
+        <Link href="/sign-up">Ainda não tem uma conta?</Link>
+      </Card>
+      
+    </main>
+  );
 }
